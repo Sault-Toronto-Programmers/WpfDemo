8ed0433579b1b1211ed3d84a2e73f2a7f0dba09d
rangapp-droid <rekprakash@gmail.com>
2021-08-25 15:20:55 -0400

    Add project files.

---
+++ WpfDemo/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(answer.Text);
+
+            if (answer.Text.Equals("3"))
+            {
+                Window2 w = new Window2(username.Text);
+                w.Show();
+            }
+            else
+            {
+                MessageBox.Show("Wrong answer !");
+                
+            }
+        }
+    }
+}
